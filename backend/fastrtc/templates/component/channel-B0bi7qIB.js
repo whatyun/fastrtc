723c61331f3d089e9c000004b741f2fd80fd94d3
Freddy Boulton <41651716+freddyaboulton@users.noreply.github.com>
2025-08-05 06:20:41 -0400

    Fix (#370)

--- backend/fastrtc/templates/component/channel-B0bi7qIB.js
+++
@@ -1,5 +0,0 @@
-import { ao as r, ap as n } from "./mermaid.core-C363K4_Y.js";
-const t = (a, o) => r.lang.round(n.parse(a)[o]);
-export {
-  t as c
-};
--- backend/fastrtc/templates/component/index.js
+++ backend/fastrtc/templates/component/index.js
@@ -1,4 +1,4 @@
-import { E as s, a as t, I as l, l as d, d as o, b as p } from "./index-DCFy4siO.js";
+import { E as s, a as t, I as l, l as d, d as o, b as p } from "./index-XMaphynx.js";
 export {
   s as BaseExample,
   t as BaseInteractiveVideo,
